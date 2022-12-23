ee7abf8a00e7c8af13bcf042fe7ada5184d47c9c
Doaa <doaa.id00@gmail.com>
2022-12-22 21:30:15 -0500

    added footer and top banner details

--- src/components/Footer/index.jsx
+++ src/components/Footer/index.jsx
@@ -3,7 +3,11 @@ import "./styles.css";
 export const Footer = () => {
     return (
         <div className="footer-container">
-           
+            <h2 className="footer-title">Dealerz.</h2>
+            <p className="txt1">Privacy Policy</p>
+            <p className="txt2">Terms and Conditions</p>
+            <p className="txt3">@2020 TanaAir Studio. All rights reserved.</p>
+            <div className="rectangle"></div>
         </div>
         
     );
--- src/components/pages/home/index.jsx
+++ src/components/pages/home/index.jsx
@@ -107,7 +107,17 @@ export const HomePage = () => {
         <div className = "home-page">
 
 
-            <div className="topBanner"></div>
+            <div className="topBanner">
+                <h1 className="bannerTitle">Home Shopping, Your choice!</h1>
+                <p className="bannerTxt">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+                <div className="circle" id="circle1"></div>
+                <div className="circle" id="circle2"></div>
+                <div className="circle" id="circle3"></div>
+                <div className="circle" id="circle4"></div>
+                <div className="circle" id="circle5"></div>
+                <div className="circle" id="circle6"></div>
+                <div className="circle" id="circle7"></div>
+            </div>
 
             <Search handleSearchUpdate={handleSearchUpdate}></Search>
 
